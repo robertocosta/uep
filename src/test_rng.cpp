1e235b2fd365cdaef9a3ab3514acb56bce54fd56
Riccardo Zanol <ricc@zanol.eu>
2017-02-01 21:31:03 +0100

    Edit plots

--- src/test_rng.cpp
+++ src/test_rng.cpp
@@ -1,57 +1,69 @@
 #include "rng.hpp"
 
 #include <iostream>
-
-#include <boost/python.hpp>
-#include <Python.h>
+#include <fstream>
 
 using namespace std;
 
-void test() {
-  fountain f(10);
-  for (int i = 0; i < 100; i++) {
-    fountain::row_type ps = f.next_row();
-    for (auto j = ps.begin(); j != ps.end(); j++) {
-      cout << *j << ' ';
-    }
-    cout << endl;
+void print_ideal(int N, int K) {
+  // Ideal soliton samples
+  ofstream ideal("ideal_soliton_samples.csv");
+  ideal.exceptions(ios_base::badbit | ios_base::failbit);
+  std::mt19937 g;
+  soliton_distribution s_dist(K);
+  ideal << "sample" << endl;
+  for (int i = 0; i < N; i++) {
+    uint_fast32_t sample = s_dist(g);
+    ideal << sample << endl;
   }
+  ideal.close();
 }
 
-PyObject *py_test(PyObject *, PyObject *) {
-  test();
-  Py_INCREF(Py_None);
-  return Py_None;
+void print_robust(int N, int K, double c, double delta) {
+  // Robust soliton samples
+  ofstream robust("robust_soliton_samples.csv");
+  robust.exceptions(ios_base::badbit | ios_base::failbit);
+  std::mt19937 g;
+  robust_soliton_distribution r_dist(K, c, delta);
+  robust << "sample" << endl;
+  for (int i = 0; i < N; i++) {
+    uint_fast32_t sample = r_dist(g);
+    robust << sample << endl;
+  }
+  robust.close();
 }
 
-static PyMethodDef test_modules[] = {
-  {"test", py_test, METH_NOARGS, "Test func"}
-};
+void print_ideal_pmd(int K) {
+  ofstream out("ideal_pmd.csv");
+  out.exceptions(ios_base::badbit | ios_base::failbit);
+  out << "degree, pmd" << endl;
+  for (int i = 1; i <= K; ++i) {
+    out << i << ", " << soliton_distribution::soliton_pmd(K,i) << endl;
+  }
+  out.close();
+}
 
-static PyModuleDef TestModule = {PyModuleDef_HEAD_INIT,
-				 "rngtest",
-				 NULL, -1, test_modules,
-    NULL, NULL, NULL, NULL};
-
-PyObject *init_test_module() {
-  return PyModule_Create(&TestModule);
+void print_robust_pmd(int K, double c, double delta) {
+  ofstream out("robust_pmd.csv");
+  out.exceptions(ios_base::badbit | ios_base::failbit);
+  out << "degree, pmd" << endl;
+  for (int i = 1; i <= K; ++i) {
+    out << i << ", " << robust_soliton_distribution::robust_pmd(K,c,delta,i) << endl;
+  }
+  out.close();
 }
 
 int main(int, char**) {
-  PyImport_AppendInittab("rngtest", &init_test_module);
-  Py_Initialize();
+  const int N = 1e4;
+  const int K = 1e4;
+  print_ideal(N,K);
+  const double c = 0.2;
+  const double delta = 0.05;
+  print_robust(N,K,c, delta);
 
-  PyObject *module_name = PyUnicode_FromString("rngtest");
-  PyObject *py_test_mod = PyImport_Import(module_name);
-  Py_DECREF(module_name);
-
-  PyObject *test_func = PyObject_GetAttrString(py_test_mod, "test");
-  PyObject_CallObject(test_func, NULL);
-
-  Py_DECREF(test_func);
-  Py_DECREF(module_name);
-
-  Py_Finalize();
+  print_ideal_pmd(K);
+  print_robust_pmd(K,c,delta);
+  
   
   return 0;
 }
