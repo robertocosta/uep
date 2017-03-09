d21b5c54634a25d1887e8ecc4b9942f684744c7a
Riccardo Zanol <ricc@zanol.eu>
2017-03-09 19:05:25 +0100

    Comment the lazy_xor class

--- src/lazy_xor.hpp
+++ src/lazy_xor.hpp
@@ -6,25 +6,35 @@
 
 namespace uep {
 
+/** Class used to delay the application of XORs on some other type. */
 template <class T>
 class lazy_xor {
 public:
+  /* Build an empty lazy_xor. */
   lazy_xor() = default;
+  /* Build a lazy_xor that using the intitial value pointed to by initial.
+   * The pointer must remain valid during the lifetime of the lazy_xor
+   * object.
+   */
   explicit lazy_xor(const T *initial) {
     to_xor.insert(initial);
   }
 
   lazy_xor(const lazy_xor &) = default;
   lazy_xor(lazy_xor &&) = default;
-  
+
   lazy_xor &operator=(const lazy_xor &) = default;
   lazy_xor &operator=(lazy_xor &&) = default;
-  
+
   void swap(lazy_xor &other) {
     using std::swap;
     swap(to_xor, other.to_xor);
   }
 
+  /** Cumulative XOR with another object.
+   *  This only merges the set of pointers to the objects that will be
+   *  XORed at eval time. It also cancels out identical pointers.
+   */
   void xor_with(const lazy_xor &other) {
     for (auto i = other.to_xor.cbegin(); i != other.to_xor.cend(); ++i) {
       auto iret = to_xor.insert(*i);
@@ -33,7 +43,10 @@ public:
       }
     }
   }
-  
+
+  /** Perform the XOR between the underlying objects and return the result.
+   *  This method throws an exception when called on empty objects
+   */
   T evaluate() const {
     if (empty()) throw std::runtime_error("Cannot evaluate empty xor-set");
     auto i = to_xor.cbegin();
@@ -44,12 +57,16 @@ public:
     return e;
   }
 
+  /** Return true when the set of objects to XOR is empty. */
   bool empty() const { return to_xor.empty(); }
+  /** Return the size of the set of objects to XOR. */
   std::size_t size() const { return to_xor.size(); }
 
+  /** Return the same as !empty(). */
   explicit operator bool() const { return !empty(); }
+  /** Return the same as empty(). */
   bool operator!() const { return empty(); }
-  
+
 private:
   std::unordered_set<const T*> to_xor;
 };
