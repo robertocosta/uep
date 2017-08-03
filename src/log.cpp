8a386b0a95199cc71585d06a85f8ee0667a68e3b
Riccardo Zanol <ricc@zanol.eu>
2017-08-03 12:05:02 +0200

    Rewrite the logging functions

---
+++ src/log.cpp
@@ -0,0 +1,84 @@
+#include "log.hpp"
+
+namespace expr = boost::log::expressions;
+namespace keywords = boost::log::keywords;
+namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
+namespace src = boost::log::sources;
+
+namespace uep {
+namespace log {
+
+static bool was_inited = false;
+
+void init() {
+  init("/dev/null");
+}
+
+void init(const std::string &perflog) {
+  if (was_inited) throw std::runtime_error("uep::log::init was called twice");
+  else was_inited = true;
+
+  logging::add_common_attributes();
+
+  auto filter_perf =  expr::attr<channel_type>("Channel") == performance;
+  auto filter_basic =  expr::attr<channel_type>("Channel") == basic;
+
+  auto formatter_perf = expr::stream
+    << expr::format_date_time<boost::posix_time::ptime>("TimeStamp",
+							"[%Y-%m-%d %H:%M:%S.%f] ")
+    // << expr::format_named_scope("Scopes", "(%n) ")
+    << "| " << expr::message;
+
+  auto formatter_basic = expr::stream
+    << expr::format_date_time<boost::posix_time::ptime>("TimeStamp",
+							"[%Y-%m-%d %H:%M:%S.%f] ")
+    // << expr::format_named_scope("Scopes", "(%n) ")
+    << expr::attr<severity_level>("Severity") << " "
+    << "| " << expr::message;
+
+  auto file_sink = logging::add_file_log(perflog);
+  file_sink->set_filter(filter_perf);
+  file_sink->locked_backend()->auto_flush(true);
+  file_sink->set_formatter(formatter_perf);
+
+  auto console_sink = logging::add_console_log();
+  console_sink->set_filter(filter_basic);
+  console_sink->locked_backend()->auto_flush(true);
+  console_sink->set_formatter(formatter_basic);
+}
+
+std::ostream &operator<<(std::ostream &strm, severity_level level) {
+  static const char *strings[] =
+    {
+      "trace",
+      "debug",
+      "info",
+      "warning",
+      "error"
+    };
+
+  if (static_cast<std::size_t>(level) < sizeof(strings) / sizeof(*strings))
+    strm << strings[level];
+  else
+    strm << static_cast<int>(level);
+
+  return strm;
+}
+
+std::ostream &operator<<(std::ostream &strm, channel_type level) {
+  static const char *strings[] =
+    {
+      "basic",
+      "performance"
+    };
+
+  if (static_cast<std::size_t>(level) < sizeof(strings) / sizeof(*strings))
+    strm << strings[level];
+  else
+    strm << static_cast<int>(level);
+
+  return strm;
+}
+
+}}
--- src/log.hpp
+++ src/log.hpp
@@ -1,197 +1,65 @@
-#ifndef LOG_HPP
-#define LOG_HPP
+#ifndef UEP_LOG_HPP
+#define UEP_LOG_HPP
 
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#include <boost/log/common.hpp>
+#include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/sinks.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/utility/manipulators.hpp>
-#include <boost/core/null_deleter.hpp>
+#include <boost/log/expressions/formatters/date_time.hpp>
+#include <boost/log/expressions/formatters/named_scope.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
 
+namespace uep {
+namespace log {
+
+/** Severity levels used to filter log messages. */
 enum severity_level {
+  trace,
   debug,
   info,
   warning,
   error
 };
 
-enum statistic_type {
-  scalar,
-  counter
+/** Channels used to separate performance counters from generic log
+ *  messages.
+ */
+enum channel_type {
+  basic,
+  performance
 };
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity_level)
-BOOST_LOG_ATTRIBUTE_KEYWORD(stat_stream, "StatisticStream", std::string)
-BOOST_LOG_ATTRIBUTE_KEYWORD(stat_type, "StatisticType", statistic_type)
-BOOST_LOG_ATTRIBUTE_KEYWORD(stat_value, "StatisticValue", double)
+/** Default severity-channel logger. */
+typedef boost::log::sources::severity_channel_logger<
+  severity_level,
+  channel_type> default_logger;
 
-class stat_backend;
-typedef boost::log::sources::severity_logger<severity_level> default_logger;
-typedef boost::log::sinks::synchronous_sink<stat_backend> default_stat_sink;
-typedef boost::log::sinks::synchronous_sink<
-  boost::log::sinks::text_ostream_backend> default_cerr_sink;
+/** Setup the logging library. Write the basic messages to the console
+ *  and the performance messages to the given file. Can be called only
+ *  once.
+ */
+void init(const std::string &perflog);
 
-class stat_scalar_backend :
-  public boost::log::sinks::basic_sink_backend<
-  boost::log::sinks::synchronized_feeding> {
-public:
-  void consume(const boost::log::record_view &rec) {
-    boost::log::value_ref<std::string, tag::stat_stream > name = rec[stat_stream];
-    if (!name) return;
-    boost::log::value_ref<statistic_type, tag::stat_type > type = rec[stat_type];
-    if (!type || !(type.get() == scalar)) return;
-    boost::log::value_ref<double, tag::stat_value> value = rec[stat_value];
-    if (!value) return;
-    auto i = stat_map.find(name.get());
-    if (i == stat_map.end())
-      stat_map.insert(make_pair(name.get(), std::vector<double>(1, value.get())));
-    else
-      i->second.push_back(value.get());
-  }
+/** Setup the logging library. Write the basic messages to the console
+ *  and the performance messages to `/dev/null`. Can be called only
+ *  once.
+ */
+void init();
 
-  void print_to_json(std::ostream &ostr) {
-    ostr << "{";
-    for (auto i = stat_map.cbegin(); i != stat_map.cend(); ++i) {
-      ostr << '"' << i->first << '"';
-      ostr << ":[";
-      const std::vector<double> &vec = i->second;
-      for (auto j = vec.cbegin(); j != vec.cend(); ++j) {
-	ostr << *j;
-	if (++(std::vector<double>::const_iterator(j)) != vec.cend()) ostr << ',';
-      }
-      ostr << "]";
-      if (++(decltype(stat_map)::const_iterator(i)) != stat_map.cend()) ostr << ',';
-    }
-    ostr << "}";
-    ostr.flush();
-  }
+/** Write the severity level to an ostream. */
+std::ostream &operator<<(std::ostream &strm, severity_level level);
 
-private:
-  std::unordered_map<std::string, std::vector<double> > stat_map;
-};
+/** Write the channel type to an ostream. */
+std::ostream &operator<<(std::ostream &strm, channel_type level);
 
-class stat_counter_backend :
-  public boost::log::sinks::basic_sink_backend<
-  boost::log::sinks::synchronized_feeding> {
-public:
-  void consume(const boost::log::record_view &rec) {
-    boost::log::value_ref<std::string, tag::stat_stream > name = rec[stat_stream];
-    if (!name) return;
-    boost::log::value_ref<statistic_type, tag::stat_type > type = rec[stat_type];
-    if (!type || !(type.get() == counter)) return;
-    auto i = stat_map.find(name.get());
-    if (i == stat_map.end())
-      stat_map.insert(make_pair(name.get(), 1));
-    else
-      i->second++;
-  }
-
-  void print_to_json(std::ostream &ostr) {
-    ostr << "{";
-    for (auto i = stat_map.cbegin(); i != stat_map.cend(); ++i) {
-      ostr << '"' << i->first << '"';
-      ostr << ':';
-      ostr << i->second;
-      if (++(decltype(stat_map)::const_iterator(i)) != stat_map.cend()) ostr << ',';
-    }
-    ostr << "}";
-    ostr.flush();
-  }
-
-private:
-  std::unordered_map<std::string, long> stat_map;
-};
-
-class stat_backend :
-  public boost::log::sinks::basic_sink_backend<
-  boost::log::sinks::synchronized_feeding> {
-public:
-  explicit stat_backend(const boost::shared_ptr<std::ostream> &output_stream) :
-    os(output_stream) {}
-
-  ~stat_backend() {
-    std::ostream &ostr = *os;
-    ostr << '{';
-    ostr << "\"Counters\":";
-    counter_backend.print_to_json(ostr);
-    ostr << ',';
-    ostr << "\"Scalars\":";
-    scalar_backend.print_to_json(ostr);
-    ostr << '}';
-    ostr.flush();
-  }
-
-  void consume(const boost::log::record_view &rec) {
-    boost::log::value_ref<std::string, tag::stat_stream > name = rec[stat_stream];
-    if (!name) return;
-    boost::log::value_ref<statistic_type, tag::stat_type > type = rec[stat_type];
-    if (!type) return;
-    switch (type.get()) {
-    case counter:
-      counter_backend.consume(rec);
-      break;
-    case scalar:
-      scalar_backend.consume(rec);
-      break;
-    }
-  }
-
-private:
-  boost::shared_ptr<std::ostream> os;
-  stat_scalar_backend scalar_backend;
-  stat_counter_backend counter_backend;
-};
-
-inline default_logger make_stat_logger(const std::string &name,
-				       statistic_type type) {
-  default_logger logger;
-  logger.add_attribute("StatisticStream",
-		       boost::log::attributes::constant<std::string>(name));
-  logger.add_attribute("StatisticType",
-		       boost::log::attributes::constant<statistic_type>(type));
-  return logger;
-}
-
-inline boost::shared_ptr<default_stat_sink> make_stat_sink(const boost::shared_ptr<std::ostream> &os) {
-  using boost::shared_ptr;
-
-  shared_ptr<stat_backend> backend(new stat_backend(os));
-  shared_ptr<default_stat_sink> log_sink(new default_stat_sink(backend));
-  log_sink->set_filter(boost::log::expressions::has_attr(stat_stream));
-  return log_sink;
-}
-
-inline boost::shared_ptr<default_cerr_sink> make_cerr_sink(severity_level level) {
-  using boost::shared_ptr;
-  using boost::null_deleter;
-  using boost::make_shared;
-  namespace sinks = boost::log::sinks;
-  namespace expr = boost::log::expressions;
-
-  shared_ptr<sinks::text_ostream_backend> backend =
-    make_shared<sinks::text_ostream_backend>();
-  backend->add_stream(shared_ptr<std::ostream>(&std::cerr, null_deleter()));
-
-  shared_ptr<default_cerr_sink> log_sink(new default_cerr_sink(backend));
-  log_sink->set_filter(!expr::has_attr(stat_stream) && severity >= level);
-  return log_sink;
-}
-
-#define PUT_STAT_SCALAR(lg, value)					\
-  if (true) {								\
-    BOOST_LOG_SEV(lg, debug) <<						\
-      boost::log::add_value("StatisticValue", (double)(value));		\
-  } else ((void)0)
-
-#define PUT_STAT_COUNTER(lg)						\
-  if (true) {								\
-    BOOST_LOG_SEV(lg, debug);						\
-  } else ((void)0)
+}}
 
 #endif
