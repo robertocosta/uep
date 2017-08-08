5084f6fb1bdf1ea51083b9857e03150fdf704d80
Riccardo Zanol <ricc@zanol.eu>
2017-08-08 11:18:35 +0200

    Add a script to run mp_plots on EC2

---
+++ mp_plots_ec2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# Get dependencies
+sudo apt-get update
+sudo apt-get install build-essential cmake libboost-all-dev protobuf-compiler libprotobuf-dev libpthread-stubs0-dev
+sudo apt-get install git
+
+# Clone repo
+git clone https://github.com/riccz/uep.git
+
+# Build
+cd uep
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+
+# Run
+src/mp_plots
+
+# Upload logs
+subdir_name=$(date +'%Y-%m-%d_%H-%M-%S')
+for l in *log; do
+    aws s3 cp "$l" s3://uep.zanol.eu/simulation_logs/"$subdir_name"/"$l"
+done
+
+# Shutdown
+sudo poweroff
