04aa387f071ce59c203d56362a491f846478a4e0
Riccardo Zanol <ricc@zanol.eu>
2017-08-10 17:16:04 +0200

    Add Knuth integer hashing for pointers

--- src/utils.hpp
+++ src/utils.hpp
@@ -2,6 +2,7 @@
 #define UEP_UTILS_HPP
 
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <iterator>
 
@@ -33,6 +34,18 @@ std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
   out << "\b\b]";
   return out;
 }
+
+/** Integer hashing function. */
+template <class T>
+struct knuth_mul_hasher {
+  std::size_t operator()(const T *k) const {
+    std::ptrdiff_t v = (std::ptrdiff_t)k * UINT32_C(2654435761);
+    // Right-shift v by the size difference between a pointer and a
+    // 32-bit integer (0 for x86, 32 for x64)
+    v >>= ((sizeof(std::ptrdiff_t) - sizeof(std::uint32_t)) * 8);
+    return (std::size_t)(v & UINT32_MAX);
+  }
+};
 
 }}
 
