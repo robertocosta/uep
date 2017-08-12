4acb0c46b4cda65e6a35bbddcda25ae86b95c544
Riccardo Zanol <ricc@zanol.eu>
2017-08-12 16:36:28 +0200

    Cleanup the perf test program

--- src/mp_plots.cpp
+++ src/mp_plots.cpp
@@ -1,15 +1,32 @@
+#include <cstdlib>
+
 extern "C" {
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 }
 
-#include "encoder.hpp"
-#include "decoder.hpp"
 #include "cmake_defines.hpp"
+#include "decoder.hpp"
+#include "encoder.hpp"
 
 using namespace std;
 using namespace uep;
+
+template <class Func>
+pid_t run_forked(const Func &f) {
+  pid_t pid = fork();
+  if (pid < 0) {
+    throw std::runtime_error("Fork failed");
+  }
+  else if (pid == 0) {
+    f();
+    exit(EXIT_SUCCESS);
+  }
+  else {
+    return pid;
+  }
+}
 
 packet random_pkt(size_t size) {
   static std::independent_bits_engine<std::mt19937, CHAR_BIT, unsigned char> g;
@@ -50,44 +67,52 @@ void run_enc_dec(size_t K, double c, double delta,
 }
 
 int main(int,char**) {
-  pid_t pid, pid2;
-  int status, status2;
-  pid = fork();
-  if (pid == 0) {
-    log::init("mp_plots_A.log");
-    log::default_logger perf_lg(boost::log::keywords::channel = log::performance);
-    BOOST_LOG(perf_lg) << "RUN CASE A";
-    BOOST_LOG(perf_lg) << "Git commit: " << GIT_COMMIT_SHA1;
-    run_enc_dec(10000, 0.01, 0.5, 3, 1024);
-    BOOST_LOG(perf_lg) << "DONE A";
-  }
-  else if (pid < 0) {
-    throw std::runtime_error("Fork failed");
-  }
-  else {
-    pid2 = fork();
-    if (pid2 == 0) {
+  std::vector<pid_t> children_pids;
+  pid_t pid;
+
+  pid = run_forked([]{
+      log::init("mp_plots_A.log");
+      log::default_logger perf_lg(boost::log::keywords::channel = log::performance);
+      BOOST_LOG(perf_lg) << "RUN CASE A";
+      BOOST_LOG(perf_lg) << "Git commit: " << GIT_COMMIT_SHA1;
+      run_enc_dec(10000, 0.01, 0.5, 3, 1024);
+      BOOST_LOG(perf_lg) << "DONE A";
+    });
+  children_pids.push_back(pid);
+  pid = run_forked([]{
       log::init("mp_plots_B.log");
       log::default_logger perf_lg(boost::log::keywords::channel = log::performance);
       BOOST_LOG(perf_lg) << "RUN CASE B";
       BOOST_LOG(perf_lg) << "Git commit: " << GIT_COMMIT_SHA1;
       run_enc_dec(10000, 0.03, 0.5, 3, 1024);
       BOOST_LOG(perf_lg) << "DONE B";
-    }
-    else if (pid < 0) {
-      throw std::runtime_error("Fork failed");
-    }
-    else {
+    });
+  children_pids.push_back(pid);
+  pid = run_forked([]{
       log::init("mp_plots_C.log");
       log::default_logger perf_lg(boost::log::keywords::channel = log::performance);
       BOOST_LOG(perf_lg) << "RUN CASE C";
       BOOST_LOG(perf_lg) << "Git commit: " << GIT_COMMIT_SHA1;
       run_enc_dec(10000, 0.1, 0.5, 3, 1024);
       BOOST_LOG(perf_lg) << "DONE C";
-      wait(&status);
-      wait(&status2);
+    });
+  children_pids.push_back(pid);
 
+  size_t nprocs = children_pids.size();
+  for (size_t i = 0; i < nprocs; ++i) {
+    int rc;
+    pid_t pid_done = wait(&rc);
+    if (rc == EXIT_SUCCESS) {
+      auto newend = remove(children_pids.begin(), children_pids.end(), pid_done);
+      children_pids.erase(newend, children_pids.end());
+    }
+    else {
+      throw runtime_error("A child failed");
     }
   }
+
+  if (!children_pids.empty())
+    throw runtime_error("Some child did not exit");
+
   return 0;
 }
