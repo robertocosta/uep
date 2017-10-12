a86a13cb92255474d4db697b8eef7b435b1ebd16
Riccardo Zanol <ricc@zanol.eu>
2017-10-12 11:03:42 +0200

    Remove the hardcoded stream name

--- h264_scripts/encode.sh
+++ h264_scripts/encode.sh
@@ -8,32 +8,33 @@ H264DECODE="$(realpath ../jsvm/bin/H264AVCDecoderLibTestStaticd)"
 BSEXTR="$(realpath ../jsvm/bin/BitStreamExtractorStaticd)"
 
 DATADIR="../dataset"
+STREAMNAME="stefan_cif"
 CFGDIR="$PWD"
 pushd "$DATADIR"
-cp "$CFGDIR"/stefan*.cfg ./
+cp "$CFGDIR"/${STREAMNAME}.cfg "$CFGDIR"/${STREAMNAME}_*.cfg ./
 
-"$H264ENCODE" -pf "stefan.cfg"
-"$BSEXTR" -pt "stefan_cif.trace" "stefan_cif.264"
+"$H264ENCODE" -pf "${STREAMNAME}.cfg"
+"$BSEXTR" -pt "${STREAMNAME}.trace" "${STREAMNAME}.264"
 
 # Repeat the GOPs to make a longer stream (>= 4000 frames)
 set +o pipefail
-gopoffset=$(grep 'SliceData' < "stefan_cif.trace" | sed 's/^\([x0-9a-fA-F]*\).*/\1/' | head -n1)
+gopoffset=$(grep 'SliceData' < "${STREAMNAME}.trace" | sed 's/^\([x0-9a-fA-F]*\).*/\1/' | head -n1)
 set -o pipefail
-dd if="stefan_cif.264" of="stefan_gops.264" bs=1 skip="$((gopoffset))"
+dd if="${STREAMNAME}.264" of="${STREAMNAME}_gops.264" bs=1 skip="$((gopoffset))"
 
 gopframes=90
 ntimes=$(( (4000 + gopframes - 1) / gopframes ))
-cp "stefan_cif.yuv" "stefan_cif_long.yuv"
-cp "stefan_cif.264" "stefan_cif_long.264"
+cp "${STREAMNAME}.yuv" "${STREAMNAME}_long.yuv"
+cp "${STREAMNAME}.264" "${STREAMNAME}_long.264"
 for i in $(seq 0 $((ntimes - 2)) ); do
-    cat stefan_gops.264 >> stefan_cif_long.264
-    cat stefan_cif.yuv >> stefan_cif_long.yuv
+    cat ${STREAMNAME}_gops.264 >> ${STREAMNAME}_long.264
+    cat ${STREAMNAME}.yuv >> ${STREAMNAME}_long.yuv
 done
-rm "stefan_gops.264"
+rm "${STREAMNAME}_gops.264"
 
-"$BSEXTR" -pt "stefan_cif_long.trace" "stefan_cif_long.264"
+"$BSEXTR" -pt "${STREAMNAME}_long.trace" "${STREAMNAME}_long.264"
 
-"$H264DECODE" "stefan_cif.264" "stefan_cif_out.yuv"
-"$H264DECODE" "stefan_cif_long.264" "stefan_cif_long_out.yuv"
+"$H264DECODE" "${STREAMNAME}.264" "${STREAMNAME}_out.yuv"
+"$H264DECODE" "${STREAMNAME}_long.264" "${STREAMNAME}_long_out.yuv"
 
 popd
