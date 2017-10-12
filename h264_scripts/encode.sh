33e89e54b723ebff83d5932071abc0e584efa9b1
Riccardo Zanol <ricc@zanol.eu>
2017-10-12 11:34:50 +0200

    Save logs, extract the number of frames

--- h264_scripts/encode.sh
+++ h264_scripts/encode.sh
@@ -9,21 +9,21 @@ BSEXTR="$(realpath ../jsvm/bin/BitStreamExtractorStaticd)"
 
 DATADIR="../dataset"
 STREAMNAME="stefan_cif"
+LONG_FRAMENO=4000
 CFGDIR="$PWD"
 pushd "$DATADIR"
 cp "$CFGDIR"/${STREAMNAME}.cfg "$CFGDIR"/${STREAMNAME}_*.cfg ./
 
-"$H264ENCODE" -pf "${STREAMNAME}.cfg"
-"$BSEXTR" -pt "${STREAMNAME}.trace" "${STREAMNAME}.264"
+frameno=$("$H264ENCODE" -pf "${STREAMNAME}.cfg" 2>&1 | tee "${STREAMNAME}_encode.log" | grep "Frames:" | sed 's/^.*Frames: \([[:digit:]]*\)$/\1/')
+"$BSEXTR" -pt "${STREAMNAME}.trace" "${STREAMNAME}.264" > "${STREAMNAME}_trace.log" 2>&1
 
-# Repeat the GOPs to make a longer stream (>= 4000 frames)
+# Repeat the GOPs to make a longer stream (>= LONG_FRAMENO frames)
 set +o pipefail
 gopoffset=$(grep 'SliceData' < "${STREAMNAME}.trace" | sed 's/^\([x0-9a-fA-F]*\).*/\1/' | head -n1)
 set -o pipefail
 dd if="${STREAMNAME}.264" of="${STREAMNAME}_gops.264" bs=1 skip="$((gopoffset))"
 
-gopframes=90
-ntimes=$(( (4000 + gopframes - 1) / gopframes ))
+ntimes=$(( (LONG_FRAMENO + frameno - 1) / frameno ))
 cp "${STREAMNAME}.yuv" "${STREAMNAME}_long.yuv"
 cp "${STREAMNAME}.264" "${STREAMNAME}_long.264"
 for i in $(seq 0 $((ntimes - 2)) ); do
@@ -32,9 +32,9 @@ for i in $(seq 0 $((ntimes - 2)) ); do
 done
 rm "${STREAMNAME}_gops.264"
 
-"$BSEXTR" -pt "${STREAMNAME}_long.trace" "${STREAMNAME}_long.264"
+"$BSEXTR" -pt "${STREAMNAME}_long.trace" "${STREAMNAME}_long.264" > "${STREAMNAME}_long_trace.log" 2>&1
 
-"$H264DECODE" "${STREAMNAME}.264" "${STREAMNAME}_out.yuv"
-"$H264DECODE" "${STREAMNAME}_long.264" "${STREAMNAME}_long_out.yuv"
+"$H264DECODE" "${STREAMNAME}.264" "${STREAMNAME}_out.yuv" > "${STREAMNAME}_decode.log" 2>&1
+"$H264DECODE" "${STREAMNAME}_long.264" "${STREAMNAME}_long_out.yuv" > "${STREAMNAME}_long_decode.log" 2>&1
 
 popd
