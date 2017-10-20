76d1affa470edeb7efc481a359e4964a9f2b6b13
Riccardo Zanol <ricc@zanol.eu>
2017-10-20 13:03:39 +0200

    Add EC2 scripts for the BER

---
+++ run_ber.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -eu -o pipefail
+set -x
+
+# Get dependencies
+sudo apt-get update
+sudo apt-get install -y \
+     build-essential \
+     cmake \
+     git \
+     libboost-all-dev \
+     libprotobuf-dev \
+     libpthread-stubs0-dev \
+     p7zip \
+     protobuf-compiler
+
+# Clone repo
+git clone --recursive https://github.com/riccz/uep.git
+
+# Build
+cd uep
+git checkout master
+mkdir -p build
+pushd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+popd
+
+# Prepare video
+pushd dataset
+wget 'http://trace.eas.asu.edu/yuv/stefan/stefan_cif.7z'
+7z x stefan_cif.7z
+popd
+pushd h264_scripts
+./encode.sh
+popd
+
+# Run
+pushd build/bin
+./server -K '[50, 950]' -R '[1, 1]' -E 1 -n 1080 > server_console.log 2>&1 &
+./client -n stefan_cif_long -t 30 -p 0 > client_console.log 2>&1 &
+wait
+popd
+
+# Process output video
+pushd h264_scripts
+./decode.sh
+popd
+
+# Upload logs
+pushd build/bin
+subdir_name=$(date +'%Y-%m-%d_%H-%M-%S')
+for l in *.log; do
+    aws s3 cp "$l" s3://uep.zanol.eu/simulation_logs/"$subdir_name"/"$l"
+done
+# Upload videos
+pushd dataset_client
+aws s3 cp "stefan_cif_long.264" s3://uep.zanol.eu/simulation_videos/"$subdir_name"/"stefan_cif_long.264"
+popd
+popd
+
+aws sns publish \
+    --region 'us-east-1' \
+    --topic-arn 'arn:aws:sns:us-east-1:402432167722:NotifyMe' \
+    --message "run_ber is finished ($subdir_name)"
+
+# Shutdown
+sudo poweroff
---
+++ run_on_ec2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+set -x
+
+script="./run_ber.sh"
+script_base64=$(base64 "$script")
+
+./launch_ec2.sh <<EOF
+set -eux -o pipefail
+base64 -d <<< "${script_base64}" > "${script}"
+chmod a+x "${script}"
+sudo apt-get update
+sudo apt-get install -y screen
+screen -d -m "${script}"
+EOF
