77d44815f595572170f4abeebf5f059497b048ec
Riccardo Zanol <ricc@zanol.eu>
2017-10-24 18:46:39 +0200

    Compute the total overhead

--- src/overhead.py
+++ src/overhead.py
@@ -56,6 +56,18 @@ class overhead_scanner:
                                       "padding=(\d+).*"
                                       "pkt_size=(\d+)",
                                       nal_sizes_h)
+        self.uep_padding_count = 0
+        def uep_padding_count_h(m):
+            self.uep_padding_count += int(m.group(1))
+        self.server_scanner.add_regex("uep_encoder::pad_partial_block.*"
+                                      "pad_cnt=(\d+)",
+                                      uep_padding_count_h)
+        self.uep_src_count = 0
+        def uep_src_count_h(m):
+            self.uep_src_count += 1
+        self.server_scanner.add_regex("uep_encoder::push.*"
+                                      "new_packet",
+                                      uep_src_count_h)
 
 
     def scan(self):
@@ -82,13 +94,17 @@ class overhead_scanner:
         print("Total size = {:f} MByte".format((self.tot_bl_size + self.tot_el_size) / 1024 /1024))
         self.tot_after_pad = (self.tot_bl_size + self.tot_el_size +
                               self.tot_bl_pad + self.tot_el_pad)
-        self.uep_pkt_count = self.tot_after_pad / self.uep_pktsize
+        (self.uep_pkt_count, pkt_count_rem) = divmod(self.tot_after_pad, self.uep_pktsize)
+        if (pkt_count_rem != 0):
+            print("tot_after_pad is not an integer number of UEP pkts")
         print("Total after padding {:f} MByte."
               " UEP source packets = {:d} ({:d} bytes)."
               " Padding = {:f} Mbyte.".format(self.tot_after_pad / 1024 / 1024,
                                               self.uep_pkt_count,
                                               self.uep_pktsize,
                                               (self.tot_bl_pad + self.tot_el_pad) / 1024 / 1024))
+        print("NAL padding overhead = {:f} %".format((self.tot_bl_pad + self.tot_el_pad) /
+                                                   (self.tot_bl_size + self.tot_el_size) * 100))
 
         self.avg_bl_size = np.mean(self.base_layer["sizes"])
         self.avg_el_size = np.mean(self.enhancement_layers["sizes"])
@@ -107,6 +123,21 @@ class overhead_scanner:
                                            self.avg_el_pad))
         print("BL fraction = {:e},"
               " EL fraction = {:e}".format(self.bl_frac, self.el_frac))
+
+        self.uep_padding_oh = self.uep_padding_count / self.uep_src_count
+        self.uep_header_oh = 4 / self.uep_pktsize
+        self.uep_tot_oh = ((self.uep_padding_count + self.uep_src_count) * (4 + self.uep_pktsize) /
+                           (self.uep_src_count * self.uep_pktsize))
+        print("UEP overhead: padding = {:f}%,"
+              " header = {:f}%,"
+              " total = {:f}%".format(self.uep_padding_oh * 100,
+                                      self.uep_header_oh * 100,
+                                      (self.uep_tot_oh - 1) * 100))
+
+        print("LT pkt overhead = {:f}%".format((self.udp_pktsize / self.lt_pktsize - 1) * 100))
+
+        self.total_server_oh = self.tot_udp_size / (self.tot_bl_size + self.tot_el_size) - 1
+        print("Total overhead NALs -> UDP = {:f}%".format(self.total_server_oh * 100))
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
