285b62d38077e78b38d2e3cbaf7e5aacbd896061
Riccardo Zanol <ricc@zanol.eu>
2017-10-29 21:46:04 +0100

    Use a bernoullian CI suitable for small z

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import re
 
 import numpy as np
 
+from scipy import stats
+
 class line_scanner:
     def __init__(self, fname):
         self.filename = fname
@@ -39,6 +41,26 @@ def read_trace_line(filename):
         traceline["truncatable"] = (next(fields) == "Yes")
         yield traceline
 
+def phi(n, z, alpha):
+    assert(n >= 2)
+    assert(z >= 0 and z <= n)
+    assert(alpha >= 0 and alpha <= 1)
+    n1 = 2*(z+1)
+    n2 = 2*(n-z)
+    f = stats.f.ppf(1-alpha, n1, n2)
+    return n1*f / (n2 + n1*f)
+
+def bernoulli_ci(z, n, gamma):
+    def L(z):
+        if z == 0:
+            return 0
+        else:
+            return phi(n, z-1, (1+gamma) / 2)
+
+    l = L(z)
+    u = 1 - L(n-z)
+    return (l, u)
+
 def success_err(z, n):
-    d = 1.96/n * math.sqrt(z*(1 - z/n))
-    return (d, d)
+    (l,u) = bernoulli_ci(z, n, 0.95)
+    return (-(l - z/n), u - z/n)
