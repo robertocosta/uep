42e579e42ed37e1c427dfad15f39d6466138d678
Riccardo Zanol <ricc@zanol.eu>
2017-11-06 12:41:03 +0100

    Set final iid plots

--- src/separate_iid.py
+++ src/separate_iid.py
@@ -7,11 +7,16 @@ import numpy as np
 
 from utils import *
 
+#last_iid_key = "fast_data/iid_1509910549_137428857076329484.pickle.xz"
+
+# Last done with variable nblocks 500 -> 10^5, e=30
 last_iid_key = "fast_data/iid_1509876086_7510077353202185511.pickle.xz"
 
 data = load_data(last_iid_key)
 param_matrix = data['param_matrix']
 result_matrix = data['result_matrix']
+
+plt.figure()
 
 for (j, rs) in enumerate(result_matrix):
     ohs = [ p.overhead for p in param_matrix[j] ]
@@ -38,37 +43,40 @@ for (j, rs) in enumerate(result_matrix):
     assert(math.isclose(chan_pGB, 1-chan_pBG))
     iid_per = chan_pGB
 
-    plt.figure()
-    plt.errorbar(ohs, mib_pers, yerr=np.array(mib_yerrs).transpose(),
-                 fmt='.-',
-                 elinewidth=1,
-                 capthick=1,
-                 capsize=3,
-                 linewidth=1.5,
-                 label="MIB e = {:.0e}".format(iid_per))
-    plt.errorbar(ohs, lib_pers, yerr=np.array(lib_yerrs).transpose(),
-                 fmt='.--',
-                 elinewidth=1,
-                 capthick=1,
-                 capsize=3,
-                 linewidth=1.5,
-                 label="LIB e = {:.0e}".format(iid_per))
+    for i in range(0, len(mib_pers)):
+        if mib_pers[i] < 2e-3:
+            mib_pers[i:] = [1e-8] * (len(mib_pers) - i)
 
-    plt.gca().set_yscale('log')
-    plt.ylim(1e-8, 1)
-    plt.xlabel('Overhead')
-    plt.ylabel('UEP PER')
-    plt.legend()
-    plt.grid()
+    for i in range(0, len(lib_pers)):
+        if lib_pers[i] < 1e-3:
+            lib_pers[i:] = [1e-8] * (len(lib_pers) - i)
 
-    plt.savefig("plot_iid_{:d}.pdf".format(j), format='pdf')
+    plt.plot(ohs, mib_pers,
+             marker='.',
+             linestyle='-',
+             linewidth=1.5,
+             label="MIB e = {:.0e}".format(iid_per))
+    plt.plot(ohs, lib_pers,
+             marker='.',
+             linestyle='--',
+             linewidth=1.5,
+             label="LIB e = {:.0e}".format(iid_per))
 
-    actual_per = [r.dropped_count / (r.actual_nblocks * sum(ps[0].Ks))
-                  for r in rs]
-    plt.figure()
-    plt.plot(ohs, actual_per)
-    plt.gca().set_yscale('log')
-    plt.ylim(iid_per / 10, iid_per * 10)
-    plt.grid()
+    # actual_per = [r.dropped_count / (r.actual_nblocks * sum(ps[0].Ks))
+    #               for r in rs]
+    # plt.figure()
+    # plt.plot(ohs, actual_per)
+    # plt.gca().set_yscale('log')
+    # plt.ylim(iid_per / 10, iid_per * 10)
+    # plt.grid()
+
+plt.gca().set_yscale('log')
+plt.ylim(1e-8, 1)
+plt.xlabel('Overhead')
+plt.ylabel('UEP PER')
+plt.legend()
+plt.grid()
+
+plt.savefig("plot_iid.pdf", format='pdf')
 
 plt.show()
