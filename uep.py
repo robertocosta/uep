7847004f82b72ebe95dc464270ba9da6a0fd714d
Riccardo Zanol <ricc@zanol.eu>
2017-11-08 23:51:56 +0100

    Add function to run in parallel

--- uep.py
+++ uep.py
@@ -1,6 +1,7 @@
+import concurrent.futures as cf
+import copy
 import math
-import matplotlib.pyplot as plt
-import numpy as np
+import os
 import random
 
 from mppy import *
@@ -17,9 +18,6 @@ class UEPSimulation:
                                      EF=kwargs.get('EF', 1),
                                      c=kwargs['c'],
                                      delta=kwargs['delta'])
-        self.__mpctx = mp_context(self.__rowgen.K)
-
-        self.__results = dict()
 
     @property
     def Ks(self):
@@ -50,14 +48,15 @@ class UEPSimulation:
         results['error_counts'] = [0 for k in self.Ks]
 
         n = math.ceil(self.K * (1 + self.overhead))
+        mpctx = mp_context(self.__rowgen.K)
         for nb in range(self.nblocks):
-            self.__mpctx.reset()
+            mpctx.reset()
 
             for l in range(n):
-                self.__mpctx.add_output(self.__rowgen())
+                mpctx.add_output(self.__rowgen())
 
-            self.__mpctx.run()
-            dec = self.__mpctx.input_symbols()
+            mpctx.run()
+            dec = mpctx.input_symbols()
 
             offset = 0
             for i, k in enumerate(self.Ks):
@@ -70,24 +69,56 @@ class UEPSimulation:
                                   for e,k in z_ek]
         return results
 
+def run_parallel(sim):
+    ncpu = len(os.sched_getaffinity(0))
+    split_nb = [math.floor(sim.nblocks / ncpu) for i in range(ncpu)]
+    for i in range(sim.nblocks % ncpu):
+        split_nb[i] += 1
+
+    result_futures = list()
+    print("Starting parallel UEPSimulations"
+          " on {:d} CPUs".format(ncpu))
+    print("Split nblocks = {:d} -> {!s}".format(sim.nblocks, split_nb))
+    with cf.ProcessPoolExecutor(ncpu) as executor:
+        sim_copy = copy.deepcopy(sim)
+        for nb in split_nb:
+            sim_copy.nblocks = nb
+            f = executor.submit(sim_copy.run)
+            result_futures.append(f)
+    print("Done")
+
+    results = dict()
+    results['error_counts'] = [0 for k in sim.Ks]
+    results['error_rates'] = [0 for k in sim.Ks]
+    for i, fs in enumerate(result_futures):
+        r = f.result()
+        for j, k in enumerate(sim.Ks):
+            results['error_counts'][j] += r['error_counts'][j]
+            results['error_rates'][j] += (r['error_rates'][j] *
+                                          split_nb[j] / sim.nblocks)
+    return results
+
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
     Ks = [100, 900]
     RFs = [3,1]
     EF = 4
     c = 0.1
     delta = 0.5
 
-    nblocks = 50
+    nblocks = 100
 
     sim = UEPSimulation(Ks=Ks, RFs=RFs, EF=EF, c=c, delta=delta,
                         nblocks=nblocks)
 
-    overheads = np.linspace(0.2, 0.25, 16)
+    overheads = np.linspace(0, 0.2, 16)
 
     avg_pers = np.zeros((len(overheads), 2))
     for j, oh in enumerate(overheads):
         sim.overhead = oh
-        results = sim.run()
+        results = run_parallel(sim)
         avg_pers[j,:] = results['error_rates']
 
     plt.figure()
