4fc35cec3312fc1893a9a85b436edd11467f3e30
Riccardo Zanol <ricc@zanol.eu>
2017-11-09 13:58:04 +0100

    Split the plots by distribution parameters

--- plot_uep_iid.py
+++ plot_uep_iid.py
@@ -7,17 +7,26 @@ if __name__ == "__main__":
     data = load_data_prefix("uep_iid/uep_vs_oh_iid_")
     print("Using {:d} data packs".format(len(data)))
 
-    Ks_set = sorted(set(tuple(d['Ks']) for d in data))
+    param_set = sorted(set((tuple(d['Ks']),
+                            tuple(d['RFs']),
+                            d['EF'],
+                            d['c'],
+                            d['delta']) for d in data))
 
-    for Ks in Ks_set:
-        data_sameKs = [d for d in data if tuple(d['Ks']) == Ks]
+    for params in param_set:
+        data_same_pars = [d for d in data if (tuple(d['Ks']) == params[0] and
+                                              tuple(d['RFs']) == params[1] and
+                                              d['EF'] == params[2] and
+                                              d['c'] == params[3] and
+                                              d['delta'] == params[4])]
+        Ks = params[0]
 
-        overheads = sorted(set(o for d in data_sameKs for o in d['overheads']))
+        overheads = sorted(set(o for d in data_same_pars for o in d['overheads']))
         avg_pers = np.zeros((len(overheads), len(Ks)))
         nblocks = np.zeros(len(overheads))
         for i, oh in enumerate(overheads):
             avg_counters = [AverageCounter() for k in Ks]
-            for d in data_sameKs:
+            for d in data_same_pars:
                 for l, d_oh in enumerate(d['overheads']):
                     if d_oh != oh: continue
                     for j,k in enumerate(Ks):
@@ -31,19 +40,31 @@ if __name__ == "__main__":
                  marker='.',
                  linestyle='-',
                  linewidth=1.5,
-                 label="MIB Ks={!s}".format(Ks))
+                 label="MIB Ks={!s},"
+                 "RFs={!s},"
+                 "EF={:d},"
+                 "c={:.3f},"
+                 "delta={:.3f}".format(*params))
         plt.plot(overheads, avg_pers[:,1],
                  marker='.',
                  linestyle='-',
                  linewidth=1.5,
-                 label="LIB Ks={!s}".format(Ks))
+                 label="LIB Ks={!s},"
+                 "RFs={!s},"
+                 "EF={:d},"
+                 "c={:.3f},"
+                 "delta={:.3f}".format(*params))
 
         plt.figure('nblocks')
         plt.plot(overheads, nblocks,
                  marker='.',
                  linestyle='-',
                  linewidth=1.5,
-                 label="Ks={!s}".format(Ks))
+                 label="Ks={!s},"
+                 "RFs={!s},"
+                 "EF={:d},"
+                 "c={:.3f},"
+                 "delta={:.3f}".format(*params))
 
     plt.figure('per')
     plt.gca().set_yscale('log')
