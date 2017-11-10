668fee443fcf27fc95caba4822204447da1adf1c
Roberto Costa <robertocosta1993@gmail.com>
2017-11-10 21:43:27 +0100

    Edit plots, add helper class

--- plot_uep_iid.py
+++ plot_uep_iid.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import datetime
+from utils import *
 from uep import *
+from plots import *
 
 if __name__ == "__main__":
     data = load_data_prefix("uep_iid/uep_vs_oh_iid_")
@@ -18,6 +20,12 @@ if __name__ == "__main__":
                             d['c'],
                             d['delta'],
                             d['iid_per']) for d in data))
+
+    p = plots()
+    p.automaticXScale = True
+    #p.automaticXScale = [0,0.3]
+    p.add_plot({'plot_name':'per','xlabel':'Overhead', 'ylabel':'PER', 'logy':True})
+    p.add_plot({'plot_name':'nblocks','xlabel':'Overhead', 'ylabel':'nblocks', 'logy':False})
 
     for params in param_set:
         data_same_pars = [d for d in data if (tuple(d['Ks']),
@@ -49,62 +57,73 @@ if __name__ == "__main__":
             nblocks[i] = avg_counters[0].total_weigth
 
         #if min(nblocks) < 10000: continue
-        #if Ks[0] < 5000: continue
+        #if Ks == (500,500): continue
         if Ks != (100,900): continue
-        if EF != 4: continue
-        if RFs[0] not in [3,5,6]: continue
+        #if EF != 4: continue
+        #if RFs[0] not in [3,5,6]: continue
 
         legend_str = ("Ks={!s},"
                       "RFs={!s},"
                       "EF={:d},"
-                      "c={:.3f},"
-                      "delta={:.3f},"
+                      "c={:.2f},"
+                      "delta={:.2f},"
                       "e={:.0e}").format(*params)
+        
+        p.add_data({'plot_name':'per','label':legend_str, 'type':'mib', 'x':overheads, 'y':avg_pers[:,0]})
+        #p.add_data({'plot_name':'per','label':legend_str, 'type':'lib', 'x':overheads, 'y':avg_pers[:,1]})
+        
+        p.add_data({'plot_name':'nblocks','label':legend_str, 'x':overheads, 'y':nblocks})
 
-        plt.figure('per')
-        plt.plot(overheads, avg_pers[:,0],
-                 marker='.',
-                 linestyle='-',
-                 linewidth=1.5,
-                 label="MIB " + legend_str)
-        plt.plot(overheads, avg_pers[:,1],
-                 marker='.',
-                 linestyle='-',
-                 linewidth=1.5,
-                 label="LIB " + legend_str)
+                
+        #plt.figure('per')
+        #plt.plot(overheads, avg_pers[:,0],
+        #         marker='.',
+        #         linestyle='-',
+        #         linewidth=1.5,
+        #         label="MIB " + legend_str)
+        #plt.plot(overheads, avg_pers[:,1],
+        #         marker='.',
+        #         linestyle='-',
+        #         linewidth=1.5,
+        #         label="LIB " + legend_str)
 
-        plt.figure('nblocks')
-        plt.plot(overheads, nblocks,
-                 marker='.',
-                 linestyle='-',
-                 linewidth=1.5,
-                 label=legend_str)
+        #plt.figure('nblocks')
+        #plt.plot(overheads, nblocks,
+        #         marker='.',
+        #         linestyle='-',
+        #         linewidth=1.5,
+        #         label=legend_str)
 
-        the_oh_is = [i for i,oh in enumerate(overheads)
-                     if math.isclose(oh, 0.24)]
-        if len(the_oh_is) > 0:
-            the_oh_i = the_oh_is[0]
-            print("At overhead {:.2f}:".format(overheads[the_oh_i]))
-            print(" " * 4 + legend_str, end="")
-            print(" -> MIB={:e}, LIB={:e}".format(avg_pers[the_oh_i, 0],
-                                                  avg_pers[the_oh_i, 1]))
+        #the_oh_is = [i for i,oh in enumerate(overheads)
+        #             if math.isclose(oh, 0.24)]
+        #if len(the_oh_is) > 0:
+        #    the_oh_i = the_oh_is[0]
+        #    print("At overhead {:.2f}:".format(overheads[the_oh_i]))
+        #    print(" " * 4 + legend_str, end="")
+        #    print(" -> MIB={:e}, LIB={:e}".format(avg_pers[the_oh_i, 0],
+        #                                          avg_pers[the_oh_i, 1]))
 
-    plt.figure('per')
-    plt.gca().set_yscale('log')
-    plt.ylim(1e-8, 1)
-    plt.xlabel('Overhead')
-    plt.ylabel('UEP PER')
-    plt.legend()
-    plt.grid()
+    #per_plot = plt.figure('per')
+    #plt.gca().set_yscale('log')
+    #plt.ylim(1e-8, 1)
+    #plt.xlabel('Overhead')
+    #plt.ylabel('UEP PER')
+    #plt.legend()
+    #plt.grid()
 
-    plt.savefig('uep_iid_per.pdf', format='pdf')
+    datestr = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    save_plot_png(p.get_plot('per'),'iid '+p.describe_plot('per')+datestr)
+    save_plot_png(p.get_plot('nblocks'),'iid '+p.describe_plot('nblocks')+datestr)
 
-    plt.figure('nblocks')
-    plt.xlabel('Overhead')
-    plt.ylabel('nblocks')
-    plt.legend()
-    plt.grid()
+    #plt.savefig('uep_iid_per.pdf', format='pdf')
 
-    plt.savefig('uep_iid_nblocks.pdf', format='pdf')
+    #nblocks_plot = plt.figure('nblocks')
+    #plt.xlabel('Overhead')
+    #plt.ylabel('nblocks')
+    #plt.legend()
+    #plt.grid()
 
-    plt.show()
+    #plt.savefig('uep_iid_nblocks.pdf', format='pdf')
+
+    #save_plot_png(nblocks_plot,'iid_nblocks_'+datestr)
+    #plt.show()
---
+++ plots.py
@@ -0,0 +1,174 @@
+import matplotlib.pyplot as plt
+import math
+
+class plots:
+
+    def __init__(self):
+        self.__plots = []
+        self.__plot_ind = 0
+        self.__nplots = 0
+        self.minExp = -16
+        self.__minY = math.pow(10,self.minExp)
+        self.__automaticXScale = True
+
+    def __check_name(self,name):
+        names = [self.__plots[i]['name'] for i in range(self.__nplots)]
+        if (name in names):
+            self.__plot_ind = names.index(name)
+            return True
+        else:
+            return False
+    
+    @property
+    def automaticXScale(self):
+        return self.__automaticXScale
+    @automaticXScale.setter
+    def automaticXScale(self,value):
+        self.__automaticXScale = value
+    
+    @property
+    def toStr(self):
+        s = 'plots:\n'
+        names = [self.__plots[i]['name'] for i in range(self.__nplots)]
+        for i in range(len(names)):
+            s += str(i) + ' : ' + names[i] + '\n'
+        return s
+    
+    def add_plot(self,params): # params: { plot_name, xlabel, ylabel, logy}
+        plot_name = ''
+        if ('plot_name' in params):
+            plot_name = params['plot_name']
+        else:
+            print('Error: no field \'plot_name\' specified')
+            exit()
+        xlabel = ''
+        ylabel = ''
+        if ('xlabel' in params):
+            xlabel = params['xlabel']
+        else:
+            print('Error: no field \'xlabel\' specified')
+            exit()
+        if ('ylabel' in params):
+            ylabel = params['ylabel']
+        else:
+            print('Error: no field \'ylabel\' specified')
+            exit()
+        logy = False
+        if ('logy' in params): 
+            if (params['logy']==True):
+                logy = True
+
+             
+        self.__plots.append({   'name':plot_name, 
+                                'xmin': 0, 
+                                'xmax': 0,
+                                'ymin': 0.1*logy, 
+                                'ymax': self.__minY,
+                                'xlabel': xlabel,
+                                'ylabel': ylabel,
+                                'logy': logy,
+                                'obj':plt.figure(plot_name)})
+        self.__nplots += 1
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        if (logy): 
+            plt.gca().set_yscale('log')
+
+    def add_data(self,params): #params = {'plot_name', 'x', 'y', 'label', 'type' in ['mib', 'lib']}
+        p = self.__plots
+        if ('plot_name' in params):
+            plt_name = params['plot_name']
+            if (self.__check_name(plt_name)):
+                plt.figure(plt_name)
+            else:
+                print('Plot not yet created. Please use add_plot first')
+                exit()
+        else:
+            print('Error: \'plot_name\' is necessary')
+            exit()
+        if ((('x' in params) * ('y' in params) * (len(params['x'])==len(params['y'])))==False):
+            print('Error: x, y array are necessary and they must be of the same length')
+            exit()
+        if ('label' in params == False):
+            print('Error: label is a necessary fields')   
+            exit()
+        this_type = ''
+        if ('type' in params):
+            this_type = params['type']
+
+        p = p[self.__plot_ind]
+
+        y = params['y']
+        x = params['x']
+
+        if (p['logy'] == True):
+            if (min(y) <= 0):
+                print('Error: impossible to plot negative value in log scale.')
+                print('approximating 0 with {:.1e}'.format(self.__minY))
+                y_positive = y[[i for i in range(len(y)) if y[i]>0]]
+                #assert(all(y_positive[i]>0 for i in range(len(y_positive))))
+                #print('y_min = {:.1e}, p[\'y_min\'] = {:.1e}'.format(min(y_positive),p['ymin']))
+                p['ymin'] = min([p['ymin'],min(y_positive)])
+                y[[i for i in range(len(y)) if y[i]<=0]] = self.__minY
+            else:
+                p['ymin'] = min([p['ymin'],min(y)])
+        else:
+            p['ymin'] = min([p['ymin'],min(y)])
+
+        if (self.__automaticXScale == True):
+            p['xmin'] = min([p['xmin'],min(x)])
+            p['xmax'] = max([p['xmax'],max(x)])
+        else:
+            p['xmin'] = self.__automaticXScale[0]
+            p['xmax'] = self.__automaticXScale[1]
+
+        p['ymax'] = max([p['ymax'],max(y)])
+        ls = '-'
+        if (this_type == 'lib'): ls += '-'
+
+        plt.plot(x, y, marker='.',linewidth=0.5,linestyle=ls,label=(this_type + params['label']))
+        plt.xlim(p['xmin'], p['xmax']+math.fabs(p['xmax'])/100)
+        plt.ylim(p['ymin'], p['ymax'] + math.fabs(p['ymax'])/100)
+        plt.grid()
+        plt.legend()
+
+    def get_plot_obj(self, name):
+        if (name in ['*', '']):
+            return self.__plots
+        if (self.__check_name(name)==True):
+            return self.__plots[self.__plot_ind]
+        else:
+            print('No plot named '+name)
+    def get_plot(self, name):
+        if (name in ['*', '']):
+            return self.__plots
+        if (self.__check_name(name)==True):
+            return self.__plots[self.__plot_ind]['obj']
+        else:
+            print('No plot named '+name)
+
+    def show_plot(self, name):
+        if (self.__check_name(name)==True):
+            plt.show()
+        else:
+            print('No plot named '+name)
+
+    def describe_plot(self, name):
+        if (self.__check_name(name)==True):
+            p = self.__plots[self.__plot_ind]
+            return p['ylabel'] + ' VS ' + p['xlabel'] + ' '
+        else:
+            print('No plot named '+name)
+
+if __name__ == '__main__':
+    import numpy as np
+    p = plots()
+    p.add_plot({'plot_name':'first','xlabel':'Overhead', 'ylabel':'Packet error rate', 'logy':True})
+    p.add_data({'plot_name':'first','label':'label1', 'type':'mib',
+                'x':np.array([0.1, 0.2, 0.3]),
+                'y':np.array([10, 103, 108])})
+    p.add_data({'plot_name':'first','label':'label1', 'type':'lib',
+                'x':np.array([0.1, 0.2, 0.3]),
+                'y':np.array([1, 100, 104])})
+    print('Plotting:' + p.describe_plot('first'))
+    p.show_plot('first')
