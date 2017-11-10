a5cb4d4b09149d35c5b85c8004f6c472793196c9
Roberto Costa <robertocosta1993@gmail.com>
2017-11-10 11:26:57 +0100

    .

--- setup_dei.sh
+++ setup_dei.sh
@@ -3,14 +3,17 @@
 set -eu -o pipefail
 
 dei_user=${1}
-
+if [ "$dei_user" == 'costarob' ]; then
+make -j$(( $(nproc) + 1))
+else
 pushd build
 make -j$(( $(nproc) + 1))
 popd
+fi
 
 ssh ${dei_user}@login.dei.unipd.it 'mkdir -p ~/uep_run'
 scp \
-    build/lib/mppy.so \
+    lib/mppy.so \
     run_uep_iid.job \
     run_uep_iid.py \
     uep.py \
