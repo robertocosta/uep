1ed4b4556e75a4aef359cbb542d6175fb122b274
Roberto Costa <robertocosta1993@gmail.com>
2017-11-14 16:07:40 +0100

    job_script

--- run_many_iid.sh
+++ run_many_iid.sh
@@ -2,19 +2,47 @@
 
 set -eu -o pipefail
 
-rfs=(1 3 4 5)
-efs=(1 2)
-nblocks=10000
-echo -e "#!/bin/bash" > ./run_uep_iid.job
-echo -e "#$ -cwd -m ea"  >> ./run_uep_iid.job
+jobName=(iid_1 iid_2)
+nblocks=(128 640 6400)
 
-for rf in ${rfs[*]}
-do
-    for ef in ${efs[*]}
+function writeJob {
+    pers=$1
+    rfs=$2
+    efs=$3
+    nblocks=$4
+    jn=$5
+    jobN=$jn.$nblocks.job
+    echo -e "#!/bin/bash" > ./$jobN
+    echo -e "#$ -cwd -m ea"  >> ./$jobN
+
+    for per in ${pers[*]}
     do
-        printf "python3 run_uep_iid.py %s %s %s\n" $rf $ef $nblocks >> ./run_uep_iid.job
-        #python3 run_uep_iid.py $rf $ef $nblocks
+        for rf in ${rfs[*]}
+        do
+            for ef in ${efs[*]}
+            do
+                printf "python3 run_uep_iid.py --iid_per  %s %s %s %s\n" $per $rf $ef $nblocks >> ./$jobN
+                #python3 run_uep_iid.py $rf $ef $nblocks
+            done
+        done
+    done
+    qsub $jobN
+}
+
+for nbl in ${nblocks[*]}
+do
+    for jn in ${jobName[*]}
+    do
+        if [ $jn=iid_1 ]; then
+            rfs=(1 5)
+            efs=(1 2 4)
+            pers=(0)
+            writeJob $pers $rfs $efs $nbl $jn
+        else
+            rfs=(5)
+            efs=(2)
+            pers=(0.01 0.1 0.2 0.3)
+            writeJob $pers $rfs $efs $nbl $jn
+        fi
     done
 done
-
-qsub run_uep_iid.job
--- run_many_markov.sh
+++ run_many_markov.sh
@@ -2,26 +2,66 @@
 
 set -eu -o pipefail
 
-rfs=(1 3)
-efs=(1 3)
-#EnG=(960 999)
-EnG=(960)
-max_corr=1000
-nblocks=640
+jobName=(mar_1 mar_2 mar_3 mar_4)
+nblocks=(128 640 6400)
 
-echo -e "#!/bin/bash" > ./run_uep_markov.job
-echo -e "#$ -cwd -m ea"  >> ./run_uep_markov.job
+function writeJob {
+    pis=$4
+    rfs=$1
+    efs=$2
+    nblocks=$3
+    enb=$5
+    jn=$6
+    jobN=$jn.$nblocks.job
+    echo -e "#!/bin/bash" > ./$jobN
+    echo -e "#$ -cwd -m ea"  >> ./$jobN
 
-for eng in ${EnG[*]}
-do
-    let "enb = $max_corr - $eng"
-    for rf in ${rfs[*]}
+    for pib in ${pis[*]}
     do
-        for ef in ${efs[*]}
+        for rf in ${rfs[*]}
         do
-            printf "python3 run_uep_markov.py %s %s %s %s %s\n" $rf $ef $nblocks $eng $enb >> ./run_uep_markov.job
-            #python3 run_uep_iid.py $rf $ef $nblocks
+            for ef in ${efs[*]}
+            do
+                printf "python3 run_uep_markov.py --overhead 0.3 %s %s %s %s %s\n" $rf $ef $nblocks $pib $enb >> ./$jobN
+                #python3 run_uep_iid.py $rf $ef $nblocks
+            done
         done
     done
+    qsub $jobN
+}
+
+for nbl in ${nblocks[*]}
+do
+    for jn in ${jobName[*]}
+    do
+        if [ $jn=mar_1 ]; then
+            rfs=(1 3 5)
+            efs=(1)
+            pib=(inf)
+            enb=(1)
+            writeJob $rfs $efs $nbl $pib $enb $jn
+        else
+            if [ $jn=mar_2 ]; then
+                rfs=(1 3 5)
+                efs=(1)
+                pib=(0.01 0.1)
+                enb=(5)
+                writeJob $rfs $efs $nbl $pib $enb $jn
+            else
+                if [ $jn=mar_3 ]; then
+                    rfs=(1 3 5)
+                    efs=(1)
+                    pib=(0.01 0.1)
+                    enb=(10)
+                    writeJob $rfs $efs $nbl $pib $enb $jn
+                else
+                    rfs=(1 3 5)
+                    efs=(1)
+                    pib=(0.01 0.1)
+                    enb=(50)
+                    writeJob $rfs $efs $nbl $pib $enb $jn
+                fi
+            fi
+        fi
+    done
 done
-#qsub run_uep_markov.job
