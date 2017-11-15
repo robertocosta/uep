9ed2809f485bdfb50775befc704f2a0a6947e922
Riccardo Zanol <ricc@zanol.eu>
2017-11-15 15:31:58 +0100

    Add functions to save in pdf

--- utils/plots.py
+++ utils/plots.py
@@ -15,10 +15,16 @@ import botocore
 
 from dateutil.tz import tzutc
 
-def save_plot_png(my_plot, name):
+def save_plot(my_plot, name, fmt):
     print_fig = my_plot
     print_fig.set_size_inches(10,8)
     print_fig.set_dpi(200)
-    fullname = "average/{}.png".format(name)
+    fullname = "average/{}.{}".format(name, fmt)
     os.makedirs(os.path.dirname(fullname), exist_ok=True)
-    print_fig.savefig(fullname, format='png')
+    print_fig.savefig(fullname, format=fmt)
+
+def save_plot_png(my_plot, name):
+    save_plot(my_plot, name, 'png')
+
+def save_plot_pdf(my_plot, name):
+    save_plot(my_plot, name, 'pdf')
